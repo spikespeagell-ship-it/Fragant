0d2537a07d65aad3acdcc328ff1a2815cbab88a7
spikespeagell-ship-it <spikespeagell@gmail.com>
2026-03-22 03:35:29 +0000

    Configurando puerto dinamico para el hosting

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ app.delete("/perfumes/:id", async (req, res) => {
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
